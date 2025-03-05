beb165a0c2071a60611c813f8a713d6863ae829f
Jeff Mbithi <jeffmbithi9@gmail.com>
2025-03-05 18:49:42 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
+  let result;
+
+  if (distance <= 400) {
+    result = "This one is on me!";
+  } else if (distance <= 2000) {
+    result = "That will be twenty bucks.";
+  } else if (distance <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else {
+    result = "No can do.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let result = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let result;
+
+  switch (tip) {
+    case "generous":
+      result = "Thank you so much.";
+      break
+    case "not as generous":
+      result = "Thank you.";
+      break
+    case "thanks for everything":
+      result = "Bye.";
+  }
+  return result
 }
